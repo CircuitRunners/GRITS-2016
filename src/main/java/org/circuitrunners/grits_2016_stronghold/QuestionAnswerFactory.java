fbd5136fbf6541b15a9f46f35cce5ffb6136d309
mastercoms <circuitsoft@tuta.io>
2016-09-20 13:30:34 -0400

    Reorder QA to appropriate positions

--- src/main/java/org/circuitrunners/grits_2016_stronghold/QuestionAnswerFactory.java
+++ src/main/java/org/circuitrunners/grits_2016_stronghold/QuestionAnswerFactory.java
@@ -5,6 +5,6 @@ package org.circuitrunners.grits_2016_stronghold;
  */
 public class QuestionAnswerFactory {
     public static String[] produceQA() {
-        return new String[]{AnswerFactory.produceAnswer(), QuestionFactory.produceQuestion()};
+        return new String[]{QuestionFactory.produceQuestion(), AnswerFactory.produceAnswer()};
     }
 }
