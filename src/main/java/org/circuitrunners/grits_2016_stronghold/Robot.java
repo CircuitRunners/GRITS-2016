f2c9f89e88257f6127e47a1a01bb601c2146caf6
colinmcmillen <jerrym08@me.com>
2016-10-22 10:52:23 -0400

    THREADS

--- src/main/java/org/circuitrunners/grits_2016_stronghold/Robot.java
+++ src/main/java/org/circuitrunners/grits_2016_stronghold/Robot.java
@@ -1,6 +1,7 @@
 package org.circuitrunners.grits_2016_stronghold;
 
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import org.circuitrunners.grits_2016_stronghold.qa.AutonomousJoystick;
 import org.circuitrunners.grits_2016_stronghold.qa.QuestionAnswerFactory;
 import org.circuitrunners.grits_2016_stronghold.system.*;
@@ -18,6 +19,8 @@ public class Robot extends IterativeRobot {
     private Joystick joystick;
     private AutonomousJoystick lsd;
     private Joystick xbox;
+
+    private RobotDriveThread robotDriveThread;
 
     @Override
     public void robotInit() {
@@ -59,26 +62,55 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void autonomousInit() {
-        //lsd = new AutonomousJoystick(5);
-        //drive.arcadeDrive(1, 0);
-        /*lsd.setRawButton(5, true);
-        systems.forEach(s -> s.run(lsd));*/
-        /*try {
+        lsd = new AutonomousJoystick(5);
+        lsd.setRawButton(5, true);
+        systems.forEach(s -> s.run(lsd));
+        try {
             Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            lsd.setRawButton(5, false);
+            systems.forEach(s -> s.run(lsd));
+        }
+        lsd.setRawButton(5, false);
+        systems.forEach(s -> s.run(lsd));
+        drive.arcadeDrive(1, 0);
+        try {
+            Thread.sleep(2500);
         } catch (InterruptedException e) {
             e.printStackTrace();
             drive.arcadeDrive(0, 0);
         }
         drive.arcadeDrive(0, 0);
-        /*lsd.setRawButton(5, false);
-        systems.forEach(s -> s.run(lsd));*/
+    }
+
+    @Override
+    public void teleopInit() {
+        if (robotDriveThread == null) {
+            robotDriveThread = new RobotDriveThread();
+        }
+        robotDriveThread.start();
     }
 
     @Override
     public void teleopPeriodic() {
-        boolean thresholdL = Math.abs(xbox.getRawAxis(1)) >= 0.05;
-        boolean thresholdR = Math.abs(xbox.getRawAxis(4)) >= 0.05;
-        drive.arcadeDrive(thresholdL ? xbox.getRawAxis(1) : 0, thresholdR ? xbox.getRawAxis(4) : 0);
-        systems.forEach(s -> s.run(s.getJoystickType() == RobotMap.JoystickType.XBOX ? xbox : joystick)); // only works with two joysticks
+        systems.parallelStream().forEach(s -> s.run(s.getJoystickType() == RobotMap.JoystickType.XBOX ? xbox : joystick)); // only works with two joysticks
+    }
+
+    private class RobotDriveThread extends Thread {
+        @Override
+        public void run() {
+            while (isOperatorControl()) {
+                boolean thresholdL = Math.abs(xbox.getRawAxis(1)) >= 0.05;
+                boolean thresholdR = Math.abs(xbox.getRawAxis(4)) >= 0.05;
+                drive.arcadeDrive(thresholdL ? xbox.getRawAxis(1) : 0, thresholdR ? xbox.getRawAxis(4) : 0);
+            }
+            interrupt();
+        }
+
+        @Override
+        public void interrupt() {
+            drive.arcadeDrive(0, 0);
+        }
     }
 }
