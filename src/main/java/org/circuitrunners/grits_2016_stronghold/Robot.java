e02141648de3005b4924e958ce35e3882ce4765c
colinmcmillen <jerrym08@me.com>
2016-10-22 11:34:09 -0400

    fixes

--- src/main/java/org/circuitrunners/grits_2016_stronghold/Robot.java
+++ src/main/java/org/circuitrunners/grits_2016_stronghold/Robot.java
@@ -1,7 +1,6 @@
 package org.circuitrunners.grits_2016_stronghold;
 
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import org.circuitrunners.grits_2016_stronghold.qa.AutonomousJoystick;
 import org.circuitrunners.grits_2016_stronghold.qa.QuestionAnswerFactory;
 import org.circuitrunners.grits_2016_stronghold.system.*;
@@ -25,36 +24,41 @@ public class Robot extends IterativeRobot {
     @Override
     public void robotInit() {
         Arrays.asList(RobotMap.values()).parallelStream().forEach(system -> {
-            Talon motor;
-            if (system.getType() == RobotMap.MapMotorType.SYSTEM_MOTOR) {
-                if (system.isOpposite()) {
-                    systems.add(new InvertedRobotSystem(system.getButtons(), system.getInverted(), system.isFlop(), system.getJoystickType(), Arrays.stream(system.getPorts()).mapToObj(Talon::new).toArray(Talon[]::new)));
-                } else {
-                    systems.add(new BasicRobotSystem(system.getButtons(), system.getInverted(), system.getJoystickType(), Arrays.stream(system.getPorts()).mapToObj(Talon::new).toArray(Talon[]::new)));
-                }
-            } else if (system.getType() == RobotMap.MapMotorType.DRIVE_MOTOR) {
-                motor = new Talon(system.getPorts()[0]);
-                if (system.getInverted()) {
-                    motor.setInverted(true);
-                }
-                driveSystem.add(motor);
-            } else if (system.getType() == RobotMap.MapMotorType.DOUBLE_SOLENOID) {
-                systems.add(new DoubleSolenoidRobotSystem(system.getButtons(), new DoubleSolenoid(1, system.getPorts()[0], system.getPorts()[1])));
-            } else if (system.getType() == RobotMap.MapMotorType.CAN) {
-                systems.add(new CANRobotSystem(system.getButtons(), system.getInverted(), system.isOpposite(), new CANTalon(system.getPorts()[0])));
-            } else if (system.getType() == RobotMap.MapMotorType.SWITCH) {
-                systems.add(new SwitchSystem(system.getButtons(), system.getJoystickType(), new Relay(system.getPorts()[0])));
-            } else {
-                System.out.println("Did you just assume my MapMotorType?!?!?! I'm " + system.getType() + "!!!!!!! [TRIGGERED]");
+            switch (system.getType()) {
+                case SYSTEM_MOTOR:
+                    if (system.isOpposite()) {
+                        systems.add(new InvertedRobotSystem(system.getButtons(), system.getInverted(), system.isFlop(), system.getJoystickType(), Arrays.stream(system.getPorts()).mapToObj(Talon::new).toArray(Talon[]::new)));
+                    } else {
+                        systems.add(new BasicRobotSystem(system.getButtons(), system.getInverted(), system.getJoystickType(), Arrays.stream(system.getPorts()).mapToObj(Talon::new).toArray(Talon[]::new)));
+                    }
+                    break;
+                case DRIVE_MOTOR:
+                    Talon motor = new Talon(system.getPorts()[0]);
+                    if (system.getInverted()) {
+                        motor.setInverted(true);
+                    }
+                    driveSystem.add(motor);
+                    break;
+                case DOUBLE_SOLENOID:
+                    systems.add(new DoubleSolenoidRobotSystem(system.getButtons(), new DoubleSolenoid(1, system.getPorts()[0], system.getPorts()[1])));
+                    break;
+                case CAN:
+                    systems.add(new CANRobotSystem(system.getButtons(), system.getInverted(), system.isOpposite(), new CANTalon(system.getPorts()[0])));
+                    break;
+                case SWITCH:
+                    systems.add(new SwitchSystem(system.getButtons(), system.getJoystickType(), new Relay(system.getPorts()[0])));
+                    break;
+                default:
+                    System.out.println("Did you just assume my MapMotorType?!?!?! I'm " + system.getType() + "!!!!!!! [TRIGGERED]");
+                    break;
             }
         });
 
         driveSystem.sort(Comparator.comparingInt(PWM::getChannel));
+        drive = new RobotDrive(driveSystem.get(0), driveSystem.get(1), driveSystem.get(2), driveSystem.get(3));
 
         joystick = new Joystick(0);
         xbox = new Joystick(1);
-
-        drive = new RobotDrive(driveSystem.get(0), driveSystem.get(1), driveSystem.get(2), driveSystem.get(3));
 
         String[] qa = QuestionAnswerFactory.produceQA();
         System.out.println("question: " + qa[0] + "\nanswer: " + qa[1]);
@@ -62,17 +66,17 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void autonomousInit() {
-        lsd = new AutonomousJoystick(5);
-        lsd.setRawButton(5, true);
+        /*lsd = new AutonomousJoystick(5);
+        lsd.setRawButton(3, true);
         systems.forEach(s -> s.run(lsd));
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-            lsd.setRawButton(5, false);
+            lsd.setRawButton(3, false);
             systems.forEach(s -> s.run(lsd));
         }
-        lsd.setRawButton(5, false);
+        lsd.setRawButton(3, false);
         systems.forEach(s -> s.run(lsd));
         drive.arcadeDrive(1, 0);
         try {
@@ -81,7 +85,7 @@ public class Robot extends IterativeRobot {
             e.printStackTrace();
             drive.arcadeDrive(0, 0);
         }
-        drive.arcadeDrive(0, 0);
+        drive.arcadeDrive(0, 0);*/
     }
 
     @Override
